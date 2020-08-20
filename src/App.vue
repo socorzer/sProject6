e90577b7ec8551883aa425164356fada1a290ad7
socorzer <jirpat0956@gmail.com>
2020-08-20 15:57:26 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+   <h1> {{total}} </h1>
+    <div class="row">
+      <div class="col-6"><game lable="ok" @cc="sum" /></div>
+      <div class="col-6"><game lable="ชอบ" @cc="sum"/></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import game from "./components/game.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    game,
+  },
+  data:function(){
+    return{
+    total: 0,
+    name:'',
+    code:'',
+    gpa:0.1,
+    }
+  },
+  methods:{
+    sum(counterValue){
+    this.total +=counterValue
+    }
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/game.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <p>HP Player {{randomCharacter(1,3)}} 
+      {{player[randomCharacter(1,3)-1].name }}
+
+    </p>
+    <hr>
+    <div id="example-1">
+      <button @click="upFunction" class="btn btn-dark">{{lable}}</button>
+      <button @click="downFunction" class="btn btn-dark ml-3">-</button>
+      <p v-bind:style="{ fontSize: fsize + 'px' }">{{ counter }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      player: [
+        { name: "Anakin", hp: "90" },
+        { name: "Ironman", hp: "20" },
+        { name: "Doraemon", hp: "10" },
+      ],
+      Monster: [
+        { name: "Darth veder", hp: "90" /*img= "#"*/},
+        { name: "Thanos", hp: "20" },
+        { name: "Darth mor", hp: "10" },
+      ],
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    randomDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    randomCharacter: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    start:function(){
+// 1. random player
+// 2. random monster
+
+    },
+    attack:function(){
+// 1. player ---> monster
+// 2. player <--- monster
+// 3. player.hp =0 ---> ?
+// 4. monster.hp =0 ---> ?
+// 5. end game
+
+    }
+  },
+};
+</script>
+
+<style>
+</style>
